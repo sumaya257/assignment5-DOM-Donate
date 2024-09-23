3d1bc6d56defca1fd5786f534e66fefcd6493c3b
sumaya257 <sumayaece19@gmai.com>
2024-09-23 20:55:53 +0600

    adding-the-donating-money-to-existing-money

--- index.js
+++ index.js
@@ -24,3 +24,32 @@
      window.location.href = 'history.html'
   });
 
+//donation amount setting
+const donateBtn = document.getElementById('donate-btn');
+
+donateBtn.addEventListener('click', function() {
+    const inputValue = parseFloat(document.getElementById('input-value').value);
+    let totalBalance = parseFloat(document.getElementById('totalDonation').innerText)
+    if (inputValue > 0 && !isNaN(inputValue)) {
+        totalBalance += inputValue;
+        // Update the donation amount
+        document.getElementById('totalDonation').innerText = totalBalance + ' BDT';
+        
+        // Update the modal with the donation amount
+        document.getElementById('modal-amount').innerText = inputValue;
+
+        // Show the modal
+        document.getElementById('successModal').classList.remove('hidden');
+
+        // Clear input value
+        document.getElementById('input-value').value = '';
+    } else {
+        // error alert for invalid or negative input
+        alert('Please enter a valid positive number.');
+    }
+});
+
+// Close modal
+document.getElementById('closeModal').addEventListener('click', function() {
+    document.getElementById('successModal').classList.add('hidden');
+});
